98fb1657ff891099c3d0292f24f7e494d0977f45
borja.velasco <borja.velasco@gigigo.com>
2020-12-30 00:54:19 +0100

    Added first likes request

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
+const axios = require('axios')
+const commandParts = require('telegraf-command-parts')
 const { Telegraf } = require('telegraf')
-require('dotenv').config()
- 
+const dotenv = require('dotenv').config()
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const instance = axios.create({
+  baseURL: 'https://api.thingiverse.com/',
+  timeout: 1000,
+  headers: { 'authorization': `Bearer ${process.env.THINGIVERSE_TOKEN}` }
+});
+
+bot.use(commandParts())
 bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.help((ctx) => ctx.reply('Check available options tapping right side button'))
+
+bot.command('likes', (ctx) => {
+
+  ctx.reply("Checking your likes... 👍")
+
+  // Make a request for a user with a given ID
+  instance.get(`users/${ctx.state.command.splitArgs[0]}/likes`)
+    .then(function (response) {
+      response.data.forEach(element => {
+          ctx.replyWithPhoto(element.preview_image)
+      })
+    })
+    .catch(function (error) {
+      return ctx.reply("Couldn't retrieve yout likes 😔")
+    })
+})
+
 bot.launch()
