263e1004ae18e85c818daa8939712de7d2821c37
borja.velasco <borja.velasco@gigigo.com>
2020-12-30 02:11:47 +0100

    Added first test buttons

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const axios = require('axios')
 const commandParts = require('telegraf-command-parts')
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup, Router } = require('telegraf')
 const dotenv = require('dotenv').config()
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 const instance = axios.create({
   baseURL: 'https://api.thingiverse.com/',
-  timeout: 1000,
+  timeout: 10000,
   headers: { 'authorization': `Bearer ${process.env.THINGIVERSE_TOKEN}` }
 });
 
@@ -19,16 +20,51 @@ bot.command('likes', (ctx) => {
 
   ctx.reply("Checking your likes... 👍")
 
-  // Make a request for a user with a given ID
   instance.get(`users/${ctx.state.command.splitArgs[0]}/likes`)
     .then(function (response) {
       response.data.forEach(element => {
-          ctx.replyWithPhoto(element.preview_image)
+        ctx.reply(element.public_url)
       })
     })
     .catch(function (error) {
       return ctx.reply("Couldn't retrieve yout likes 😔")
     })
 })
+
+bot.command('collections', (ctx) => {
+
+  ctx.reply("Checking your collections... 📚")
+
+  // Make a request for a user with a given ID
+  instance.get(`users/${ctx.state.command.splitArgs[0]}/collections`)
+    .then(function (response) {
+      const collections = Markup.inlineKeyboard(response.data.map(it =>
+        Markup.callbackButton(it.name, it.id)
+      )).extra()
+
+      return ctx.reply(
+        'Like?',
+        collections
+      )
+    })
+    .catch(function (error) {
+      return ctx.reply("Couldn't retrieve yout collections 😔")
+    })
+})
+
+
+/* const inlineMessageRatingKeyboard = Markup.inlineKeyboard([
+  Markup.callbackButton('👍', 'like'),
+  Markup.callbackButton('👎', 'dislike')
+]).extra()
+
+bot.on('message', (ctx) => ctx.telegram.sendMessage(
+  ctx.from.id,
+  'Like?',
+  inlineMessageRatingKeyboard)
+)
+
+bot.action('like', (ctx) => ctx.editMessageText('🎉 Awesome! 🎉'))
+bot.action('dislike', (ctx) => ctx.editMessageText('okey')) */
 
 bot.launch()
