a272042bdf215cbb218e1c1c0efaca06d8c09241
borja.velasco <borja.velasco@gigigo.com>
2020-12-31 20:29:55 +0100

    Changed instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name: "Borja Thingicerse Bot",
       script: "src/app.js",
-      instances: "max",
+      instances: "1",
       env: {
         NODE_ENV: "development",
       },
