74de7b598262c7f69986e1ff62dbd0f19cde0edf
borja.velasco <borja.velasco@gigigo.com>
2021-01-10 20:55:02 +0100

    Added load more button to likes

--- src/commands/likes.ts
+++ src/commands/likes.ts
@@ -1,5 +1,6 @@
-import { Extra, Telegraf } from "telegraf"
+import { Extra, Markup, Telegraf } from "telegraf"
 import { TelegrafContext } from "telegraf/typings/context"
+import { isGetAccessor } from "typescript"
 import Thingiverse from "../api/thingiverse"
 import { thingToMessage } from "../messages"
 import { Thing } from "../models/thing"
@@ -14,54 +15,53 @@ function commandLikes(bot: Telegraf<any>, thingiverse: Thingiverse) {
         const userName = Utils.removeCmd(ctx.message?.text)
 
         if (userName != '') {
-            ctx.reply("⏳ Loading your likes...")
-
-            thingiverse.getUserLikes(userName)
-                .then(async function (things) {
-                    if (things.length > 0) {
-                        ctx.reply("❤️ These are your likes")
-
-                        const thingsPages = []
-
-                        for (let i = 0; i < things.length; i += ITEMS_PER_PAGE) {
-                            thingsPages.push(things.slice(i, i + ITEMS_PER_PAGE));
-                        }
-
-                        sendPage(thingsPages[0], ctx)
-                    } else ctx.reply("0️⃣ No likes were found")
-                })
-                .catch(function (error) {
-                    ctx.reply("Couldn't retrieve yout likes 🤷‍♂️")
-                })
+            loadLikes(thingiverse, ctx, userName, 0)
         } else ctx.reply("Username was not specified 🤭")
     })
 
+    bot.action(/like (.+)/, (ctx: TelegrafContext) => {
+        const args = ctx.match[1]
+        const pageToLoad = Number(args.split(" ")[0])
+        const userName = args.split(" ")[1]
+        loadLikes(thingiverse, ctx, userName, pageToLoad)
+    })
 }
 
-async function sendPage(things: Array<Thing>, ctx: TelegrafContext) {
-    for (let index = 0; index < things.length; index++) {
-        const element = things[index];
+function loadLikes(thingiverse: Thingiverse, ctx: TelegrafContext, userName: string, page: number) {
+    ctx.reply("⏳ Loading your likes...")
 
-        const buttons = Extra.markup((m) =>
-            m.inlineKeyboard([
-                [m.callbackButton('Test', 'test')],
-                [m.callbackButton('Test 2', 'test2')]
-            ])
-        )
+    thingiverse.getUserLikes(userName)
+        .then(async function (things) {
+            if (things.length > 0) {
+                ctx.reply("❤️ These are your likes")
 
-        const args = index == things.length -1 && {
-            caption: thingToMessage(element),
-            reply_markup: buttons.reply_markup
-        } || {
-            caption: thingToMessage(element)
-        } 
+                const pages = Utils.slice(things, ITEMS_PER_PAGE)
+                const currentPage = pages[page]
 
-        ctx.telegram.sendPhoto(
-            ctx.chat.id,
-            element.thumbnail,
-            args
-        )
-    }
+                for (let index = 0; index < currentPage.length; index++) {
+                    const element = currentPage[index];
+
+                    await ctx.replyWithPhoto(
+                        element.thumbnail, {
+                        caption: thingToMessage(element)
+                    })
+                }
+
+                if (currentPage.length < ITEMS_PER_PAGE) {
+                    ctx.reply("✅ Those where all your likes")
+                } else {
+                    const loadMoreButton = Markup.inlineKeyboard([
+                        [Markup.callbackButton('🙏 Yes please!', `like ${page + 1} ${userName}`)]
+                    ]).extra()
+
+                    ctx.reply("🙋 Do you want to load more items?", loadMoreButton)
+                }
+
+            } else ctx.reply("0️⃣ No likes were found")
+        })
+        .catch(function (error) {
+            ctx.reply("Couldn't retrieve yout likes 🤷‍♂️")
+        })
 }
 
 export default commandLikes
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,15 @@ export function removeCmd(cmd?: string): string {
     if(cmd != undefined ) return cmd?.replace(/(\/\w+)\s*/, '') 
     else return ''
 }
+
+export function slice(input: Array<any>, size: number){
+    if(size < 1) throw new Error("Size cannot be lower than 1")
+
+    const output = []
+
+    for (let i = 0; i < input.length; i += size) {
+        output.push(input.slice(i, i + size));
+    }
+
+    return output
+}
